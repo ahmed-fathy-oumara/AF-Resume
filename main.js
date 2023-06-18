932b0a5bd5034075af066f19eebf33f4e0284c67
ahmed_fathy_oumara <ahmedfathyoumara@gmail.com>
2023-06-18 14:43:14 +0300

    seventh commit

--- main.js
+++ main.js
@@ -81,6 +81,34 @@
     }, false);
   }();
 
+
+  /**
+   * Lazy Loading Images
+  */
+
+  let options = {
+    threshold: 1
+  }
+
+  const observer = new IntersectionObserver(imageObserver, options);
+  function imageObserver(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const img_src = img.dataset.src;
+        console.log("Lazy loading ", img);
+        img.src = img_src;
+        observer.unobserve(img);
+      }
+    })
+  }
+
+  let imgs = document.querySelectorAll('img.lazy');
+  imgs.forEach(img => {
+    observer.observe(img);
+  })
+
+  
   /**
    * Add shadow effect to fixed to top navigation bar
   */
